92bc7d3661835a6d902787a8ea6578b20b81c8a1
whatsappplove <whatsappplove@gmail.com>
2025-04-14 14:58:59 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Simula un proceso de carga (reemplaza con tu lógica de carga real)
+    setTimeout(function() {
+        // Redirige a la página del contenido principal
+        window.location.href = 'carta.html';
+    }, 7000); // Ajusta el tiempo según sea necesario
+});
